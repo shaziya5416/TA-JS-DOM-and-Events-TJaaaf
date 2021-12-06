e853f5caef8214c7cc4cbc3d878e7f93c1190d9f
Shazia Khan <shaziya5416@gmail.com>
2021-12-06 14:10:51 +0530

    changes

--- block-BJaaep/code/script.js
+++ block-BJaaep/code/script.js
@@ -1,1 +1,19 @@
-dmom
+let allBoxes=document.querySelectorAll(".first li")
+let allBoxes2=document.querySelector(".second")
+allBoxes.forEach((elm,index)=>{
+ elm.addEventListener("click",(event)=>{
+     event.target.innerText=index+1;
+     setTimeout(()=>{
+         event.target.innerText=""
+     },5000)
+ })
+
+})
+
+allBoxes2.addEventListener("click",(event)=>{
+    let value1 =event.target.dataset.value;
+    event.target.innerText=value1;
+    setTimeout(()=>{
+        event.target.innerText=""
+    },2000)
+})
