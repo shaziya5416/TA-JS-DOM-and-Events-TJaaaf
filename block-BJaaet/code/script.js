2002414533a812e290e818b92f18e23acaa51ac1
Shazia Khan <shaziya5416@gmail.com>
2021-12-10 13:35:18 +0530

    finally done

--- block-BJaaet/code/script.js
+++ block-BJaaet/code/script.js
@@ -2,6 +2,12 @@
 //when press enter should be added a list below
 //
 //when clicking cross it must delete
+
+
+
+//create toggle
+//make the cross work with index-done
+function main(){
 let rootElm=document.querySelector("ul")
 let input=document.querySelector(`input[type="text"]`);
 let list=document.querySelector("label");
@@ -10,18 +16,17 @@ let allMovies=[];
 //for deleting movies
 
 function deleteMovie(event){
-    event.target.parentElement.remove();
+    let id=event.target.dataset.id;
+    allMovies.splice(id,1);
+    createMovies();
 }
-//Added movies to the list
-input.addEventListener("keyup",(event)=>{
-    if(event.keyCode===13){
-    allMovies.push({
-        name:event.target.value,
-        watched:false
-    })
-    }
-createMovies();
-})
+//for handling toggle
+
+function handleToggle(event){
+    let id=event.target.dataset.id;
+    allMovies[id].watched=!allMovies[id].watched;
+    createMovies();
+}
 
 //make li of the movie
     /*<ul>
@@ -41,16 +46,31 @@ inputUI.classList.add("styled-checkbox");
 inputUI.id="i";
 inputUI.checked=elm.watched;
 inputUI.type="checkbox";
+inputUI.setAttribute("data-id",i);
+inputUI.addEventListener("input",handleToggle);
 let label=document.createElement("label");
 label.for="i";
 label.innerText=elm.name;
 let button=document.createElement("button");
 button.innerText="X";
-button.addEventListener("click",deleteMovie)
+button.setAttribute("data-id",i);
+button.addEventListener("click",deleteMovie);
+
 
 li.append(inputUI,label,button);
 rootElm.append(li);
 }
 )
 }
+input.addEventListener("keyup",(event)=>{
+    if(event.keyCode===13){
+    allMovies.push({
+        name:event.target.value,
+        watched:false
+    })
+    event.target.value="";
+    }
 createMovies();
+})
+}
+main();
